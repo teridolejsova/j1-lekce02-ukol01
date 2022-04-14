d4021c250fd3a6f5184fa4803aab04cdabea608e
Tereza Dolejšová <terdoe>
2022-04-14 09:56:06 +0200

    2) Namaluj osmiuhelník, kolečko a sluníčko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -9,11 +9,85 @@ public class HlavniProgram {
 
         zofka = new Turtle();
 
+        namalujCelePrasatko(zofka);
+        //přesun na obrazec
 
-        for (int i = 0; i < 2; i++) {
-            //tělo prasátka
-            namalujTeloPrasatka(zofka);
+        zofka.turnRight(25);
+        zofka.move(120);
+        zofka.penDown();
+
+        //obrazec 1
+
+        zofka.turnLeft(90);
+
+        namalujOsmiuhelnik(zofka);
+
+        //přesun na kolečko
+
+        zofka.turnRight(90);
+        zofka.move(200);
+        zofka.turnLeft(90);
+        //kolečko
+
+        zofka.penDown();
+        namalujKolecko(zofka);
+
+        //přesun na sluníčko
+        zofka.turnRight(90);
+        zofka.move(200);
+        zofka.turnLeft(80);
+        zofka.penDown();
+        zofka.turnRight(10);
+
+        //začátek sluníčka
+        zofka.move(10);
+        zofka.turnRight(10);
+        zofka.move(10);
+        zofka.turnRight(10);
+        zofka.move(10);
+
+        //sluníčko
+
+        namalujSlunicko(zofka);
+
+    }
+
+    private void namalujSlunicko(Turtle zofka) {
+        for (int t = 0; t < 12; t++) {
+
+
+        paprsekSlunicka(zofka);
+        for (int i = 0; i < 3; i++) {
+            zofka.move(10);
+            zofka.turnRight(10);
+
         }
+    }
+    }
+
+    private void paprsekSlunicka(Turtle zofka) {
+        zofka.move(10);
+        zofka.turnLeft(90);
+        zofka.move(45);
+        zofka.turnLeft(180);
+        zofka.move(45);
+        zofka.turnLeft(90);
+    }
+
+    private void namalujKolecko(Turtle zofka) {
+        for (int i = 0; i < 36; i++) {
+
+
+            zofka.move(10);
+            zofka.turnRight(10);
+        }
+        zofka.penUp();
+    }
+
+    private void namalujCelePrasatko(Turtle zofka) {
+
+        namalujTeloPrasatka(zofka);
+
         //zadní nožky
         namalujNozkyPrasatka(zofka);
 
@@ -25,8 +99,22 @@ public class HlavniProgram {
         namalujNozkyPrasatka(zofka);
 
         hlavaPrasatka(zofka);
+
+
         ocasekPrasatka(zofka);
 
+    }
+
+
+    private void namalujOsmiuhelnik(Turtle zofka) {
+        for (int i = 0; i < 8; i++) {
+
+
+            zofka.move(30);
+            zofka.turnRight(45);
+            zofka.move(20);
+        }
+        zofka.penUp();
     }
 
     private void ocasekPrasatka(Turtle zofka) {
@@ -38,15 +126,16 @@ public class HlavniProgram {
         zofka.move(40);
         zofka.turnLeft(20);
         zofka.move(20);
-        for (int i = 0; i <8 ; i++) {
+        for (int i = 0; i < 8; i++) {
 
-        zofka.turnLeft(45);
-        zofka.move(15);
+            zofka.turnLeft(45);
+            zofka.move(15);
 
-    }
+        }
         zofka.move(30);
         zofka.turnLeft(30);
         zofka.move(20);
+        zofka.penUp();
     }
 
     private void hlavaPrasatka(Turtle zofka) {
@@ -72,9 +161,15 @@ public class HlavniProgram {
     }
 
     private void namalujTeloPrasatka(Turtle zofka) {
-        zofka.move(100);
-        zofka.turnLeft(90);
-        zofka.move(150);
-        zofka.turnLeft(90);
+        for (int i = 0; i < 2; i++) {
+
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(150);
+            zofka.turnLeft(90);
+        }
     }
 }
+
+
+
